f9630c6cd256d3a7d742aebdbc016c354667cb9f
Yann Tschudy <tschudy.yann@hotmail.com>
2023-11-27 18:04:52 +0100

    style form clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,18 +26,40 @@ export default function RootLayout({
       appearance={{
         elements: {
           rootBox: 'w-full sm:w-auto flex justify-center',
-          card: 'bg-slate-900 rounded-none sm:rounded-xl',
-          headerTitle: 'text-primary',
-          headerSubtitle: 'text-primary',
+          card: 'bg-slate-900 rounded-md',
+          headerTitle: 'font-bold text-center text-secondary dark:text-primary',
+          headerSubtitle: 'text-secondary dark:text-primary',
           socialButtonsBlockButton:
-            'text-secondary bg-primary hover:bg-primary-foreground',
-          dividerText: 'text-primary',
-          dividerLine: 'bg-primary',
-          formFieldLabel: 'text-primary',
+            'text-primary bg-secondary hover:bg-secondary-foreground dark:text-secondary dark:bg-primary dark:hover:bg-primary-foreground',
+          dividerText: 'text-secondary dark:text-primary',
+          dividerLine: 'bg-secondary dark:bg-primary',
+          formFieldLabel: 'text-secondary dark:text-primary',
           formFieldHintText: 'hidden',
           formButtonPrimary:
-            'bg-primary text-secondary hover:bg-primary-foreground',
-          footerActionText: 'text-primary',
+            'text-primary bg-secondary hover:bg-secondary-foreground dark:bg-primary dark:text-secondary dark:hover:bg-primary-foreground',
+          footerActionText: 'text-secondary dark:text-primary',
+          formFieldInfoText: 'text-secondary dark:text-primary',
+          formFieldSuccessText: 'text-secondary dark:text-primary',
+          formFieldRow: 'flex-row-reverse',
+          identityPreview:
+            'text-primary bg-secondary dark:bg-primary dark:text-secondary',
+          identityPreviewEditButtonIcon: 'text-primary dark:text-secondary',
+          formHeaderTitle: 'text-secondary dark:text-primary',
+          formHeaderSubtitle: 'text-secondary dark:text-primary',
+          otpCodeFieldInput:
+            'border-secondary focus:border-secondary-foreground dark:border-primary focus:border-primary-foreground text-secondary dark:text-primary',
+          formResendCodeLink: 'text-secondary dark:text-primary',
+          alertText__danger: 'text-destructive',
+          footerActionLink:
+            'text-primary-foreground hover:text-secondary dark:text-secondary-foreground dark:hover:text-primary',
+          formFieldAction__password:
+            'text-secondary hover:text-secondary-foreground dark:text-primary dark:hover:text-primary-foreground',
+          alternativeMethodsBlockButton:
+            'bg-secondary-foreground text-secondary hover:bg-secondary-foreground hover:opacity-80 dark:bg-[#4d4d4d] dark:text-primary hover:bg-[#4d4d4d] hover:opacity-80',
+          headerBackLink:
+            'text-primary-foreground hover:text-secondary dark:text-secondary-foreground dark:hover:text-primary border-secondary',
+          headerBackIcon:
+            'text-primary-foreground hover:text-secondary dark:text-secondary-foreground dark:hover:text-primary',
         },
       }}>
       <html
--- components/header/IconBar.tsx
+++ components/header/IconBar.tsx
@@ -14,7 +14,10 @@ const IconBar = ({ onClick }: IconBarProps) => {
       onClick={onClick}
       className={cn(
         'fixed cursor-pointer text-2xl right-7 top-3 sm:hidden hover:opacity-50 z-20 text-secondary dark:text-primary',
-        pathname === '/sign-in' || pathname === '/sign-up'
+        pathname === '/sign-in' ||
+          pathname === '/sign-up' ||
+          pathname === '/sign-in/factor-one' ||
+          pathname === '/sign-up/verify-email-address'
           ? 'text-primary dark:text-primary'
           : ''
       )}>
